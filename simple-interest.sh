d27899a017478553bf2d9a7afddaa1d135244fed
Md Siyad <69454808+mdsiyad@users.noreply.github.com>
2023-01-12 14:45:23 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Mohamed Siyad
    # Additional Authors:
-   # <your GitHub username>
+   # <mdsiyad>
 
    # Input:
    # p, principal amount
